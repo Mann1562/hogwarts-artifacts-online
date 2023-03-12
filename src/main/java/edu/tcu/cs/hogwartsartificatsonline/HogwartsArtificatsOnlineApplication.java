d61fe11e10987617478c0fd87ddc234c804bf5ac
Mann1562 <71485257+Mann1562@users.noreply.github.com>
2023-03-12 14:06:26 -0500

    This is the first commit

---
+++ src/main/java/edu/tcu/cs/hogwartsartificatsonline/HogwartsArtificatsOnlineApplication.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartificatsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsArtificatsOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsArtificatsOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/tcu/cs/hogwartsartificatsonline/HogwartsArtificatsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.hogwartsartificatsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtificatsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
